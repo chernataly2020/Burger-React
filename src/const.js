2faba14f1db4b79aba384f8c9753299339f0dbe4
Nataliia Cherkes <69033364+chernataly2020@users.noreply.github.com>
2023-04-20 00:55:59 +0300

    Add files via upload

---
+++ src/const.js
@@ -0,0 +1,5 @@
+export const API_URI = 'http://localhost:3024';
+export const POSTFIX = '/api/product';
+
+//export const API_URI = 'https://fixed-fixed-borogovia.glitch.me';
+//export const POSTFIX = '/api/product';
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import categoryReducer from './category/categorySlice.js';
+import productReducer from "./product/productSlice.js";
+import orderReducer, { localStorageMiddleware } from "./order/orderSlice.js";
 
 export const store = configureStore({
   reducer:{
     category: categoryReducer,
+    product: productReducer,
+    order: orderReducer,
   },
+
+  middleware: getDefaultMiddleware => {
+    const mdws = getDefaultMiddleware().concat(localStorageMiddleware);
+
+    console.log(mdws);
+    return mdws;
+  }
 })
